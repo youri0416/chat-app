ef9a4a1726bc76e14fb48502d186217037b2fa61
youri0416 <withuuuu0416@gmail.com>
2024-09-09 15:46:41 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,17 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
 
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id:current_user.id)
+  params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
